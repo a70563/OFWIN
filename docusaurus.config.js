b3da451b58a4662958d28e4bac7afee38ea2b816
André Vieira <a64108@ualg.pt>
2023-12-20 21:26:13 +0000

    fix 3 da sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'oficinaSidebar',
             position: 'left',
             label: 'Oficina',
           },
